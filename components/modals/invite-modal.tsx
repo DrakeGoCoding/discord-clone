68a46e5f550af7a7fe033c7ade13eb993ff21be4
DrakeGoCoding <hoanglongphi21@gmail.com>
2024-10-18 02:20:26 +0700

    invite code, server settings

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import axios from 'axios';
+import { Check, Copy, RefreshCw } from 'lucide-react';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useModal } from '@/hooks/use-modal-store';
+import { useOrigin } from '@/hooks/use-origin';
+
+export const InviteModal = () => {
+  const origin = useOrigin();
+  const { isOpen, onOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === 'invite';
+  const { server } = data;
+
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1000);
+  };
+
+  const onNew = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.patch(`/api/servers/${server?.id}/invite-code`);
+      onOpen('invite', { server: res.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl font-bold">
+            Invite Friends
+          </DialogTitle>
+        </DialogHeader>
+        <div className="p-6">
+          <Label className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+          <div className="mt-2 flex items-center gap-x-2">
+            <Input
+              className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+              value={inviteUrl}
+              readOnly
+              disabled={isLoading}
+            />
+            <Button size="icon" disabled={isLoading} onClick={onCopy}>
+              {copied ? (
+                <Check className="h-4 w-4" />
+              ) : (
+                <Copy className="h-4 w-4" />
+              )}
+            </Button>
+          </div>
+          <Button
+            variant="link"
+            size="sm"
+            className="mt-4 text-xs text-zinc-500"
+            disabled={isLoading}
+            onClick={onNew}
+          >
+            Generate a new link
+            <RefreshCw className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
---
+++ types.ts
@@ -0,0 +1,9 @@
+import type { Member, Profile, Server } from '@prisma/client';
+
+export type MemberWithProfile = Member & {
+  profile: Profile;
+};
+
+export type ServerWithMembersWithProfiles = Server & {
+  members: MemberWithProfile[];
+};
