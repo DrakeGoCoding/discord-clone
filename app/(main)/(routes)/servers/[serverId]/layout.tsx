ce3eb9e3e0b525d170391d822d548e4dc4ec3582
DrakeGoCoding <hoanglongphi21@gmail.com>
2024-10-19 00:35:37 +0700

    search seve

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -36,7 +36,7 @@ export default async function ServerIdLayout({
 
   return (
     <div className="h-full">
-      <div className="fixed inset-y-0 z-20 hidden h-full w-60 flex-col md:!flex">
+      <div className="fixed inset-y-0 z-20 hidden h-full w-60 flex-col md:flex">
         <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">{children}</main>
